7986c2c193b829b7730e453c3cd2ea360bd717ab
charlemagne <lemuelpuglisi001@gmail.com>
2019-08-07 19:08:20 +0200

    test

--- src/proxyTest.php
+++ src/proxyTest.php
@@ -6,3 +6,21 @@
 
 	// Documentation: 
 	// https://goutte.readthedocs.io/en/latest/
+
+	$client = new Client(); 
+
+	$crawler = $client->request('GET', 'https://free-proxy-list.net/uk-proxy.html'); 
+	$infos = $crawler->filter('tbody > tr')->each(function ($node){
+		// var_dump($node); 
+		foreach($node as $item)
+		{
+			echo $item->nodeValue . "\n"; 
+		}
+	}); 
+	
+
+
+
+
+	
+
